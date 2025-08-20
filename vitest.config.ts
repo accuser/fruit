5a3236f3cb84782f0c1deb13ec0c36b153870649
Matthew Gibbons <mhgibbons@me.com>
2025-08-20 07:39:27 +0000

    Stablise vitest ui

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,15 +3,20 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
+		api: {
+			host: '0.0.0.0',
+			port: 5174,
+		},
 		alias: {
 			$lib: path.resolve(__dirname, './src/lib'),
+			$test: path.resolve(__dirname, './test'),
 			$types: path.resolve(__dirname, './src/types'),
 		},
 		coverage: {
-			exclude: ['./*.ts', './src/**/index.ts', './src/**/*.test.ts', 'dist', 'test'],
+			exclude: ['*.ts', 'src/**/index.ts', 'src/**/*.test.ts', 'test/**/*.ts', 'coverage/**', 'dist/**'],
 			provider: 'v8',
 		},
-		include: ['./src/**/*.test.ts', 'test/**/*.test.ts'],
+		include: ['./src/**/*.test.ts', './test/**/*.test.ts'],
 		setupFiles: ['./test/setup.ts'],
 	},
 });
