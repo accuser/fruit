311fed661c7654ef6fdd793d2db51f1927444c7f
Matthew Gibbons <mhgibbons@me.com>
2025-08-20 07:40:45 +0000

    Refactor router implementation to simplify proxy handling and improve route management

--- src/lib/router/router.ts
+++ src/lib/router/router.ts
@@ -1,34 +1,18 @@
 import type { Method, RequestHandler, Route, Router } from '$types';
-import { als, getRequestEvent } from './als';
 import { match } from './match';
+import { route } from './route';
 
 export const router = () => {
 	const routes: Route<string | URLPatternInit>[] = [];
 
-	return new Proxy(
-		{
-			fetch: async (request, env, ctx) =>
-				als.run(
-					{
-						params: {},
-						platform: { ctx, env },
-						request,
-						url: new URL(request.url),
-					},
-					() =>
-						routes.find(([predicate]) => predicate(request, env, ctx))?.[1](getRequestEvent()) ||
-						new Response(null, { status: 404 })
-				),
-		} satisfies ExportedHandler<Env>,
-		{
-			get: (target, prop, receiver) =>
-				target[prop as keyof typeof target] ||
-				(typeof prop === 'string'
-					? <const T extends string | URLPatternInit>(input: T, handler: RequestHandler<T>): Router => {
-							routes.push(match(prop.toUpperCase() as Method, input, handler));
-							return receiver;
-						}
-					: undefined),
-		}
-	) as unknown as Router;
+	return new Proxy(route(routes), {
+		get: (target, prop, receiver) =>
+			target[prop as keyof typeof target] ||
+			(typeof prop === 'string'
+				? <const T extends string | URLPatternInit>(input: T, handler: RequestHandler<T>): Router => {
+						routes.push(match(prop.toUpperCase() as Method, input, handler));
+						return receiver;
+					}
+				: undefined),
+	});
 };
